6a23b35f2bec1af2cac0bb34ca8462940d8839a9
Vishal Maurya <vishalmaurya3112@gmail.com>
2021-02-26 19:19:36 +0530

    added simple login form

--- app/src/main/java/com/example/quizzer/MainActivity.java
+++ app/src/main/java/com/example/quizzer/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.quizzer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnlog = (Button)findViewById(R.id.btnLogin);
+        EditText username = (EditText)findViewById(R.id.editTextUserName);
+        EditText password = (EditText)findViewById(R.id.editTextUserPassword);
+
+        btnlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText(MainActivity.this,"You have Authenticated Successfully",Toast.LENGTH_LONG).show();
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
 }
